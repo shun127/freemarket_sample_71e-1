75b5c1119edc29485fa7c26eb326c267728e6fe3
kinopyyy <sss.namiki@gmail.com>
2020-05-29 00:13:23 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Basic認証によるログインの要求を全てのコントローラーで行う
-  # ルーティング設定後にBasic認証のコードを改良します(5/18)。木下
-  before_action :basic_auth, if: :production?
+  # # Basic認証によるログインの要求を全てのコントローラーで行う
+  # # ルーティング設定後にBasic認証のコードを改良します(5/18)。木下
+  # before_action :basic_auth, if: :production?
   
-  # CSRF対策
-  protect_from_forgery with: :exception
+  # # CSRF対策
+  # protect_from_forgery with: :exception
   
-  private
+  # private
 
-  # # 「production?」メソッドの定義。現在の環境が本番環境ならtrue、そうでないならfalse。「if: :production?」の記述により本番環境のみでbasic_authメソッドの実行 5/20木下
-  # def production?
-  #   Rails.env.production?
+  # # # 「production?」メソッドの定義。現在の環境が本番環境ならtrue、そうでないならfalse。「if: :production?」の記述により本番環境のみでbasic_authメソッドの実行 5/20木下
+  # # def production?
+  # #   Rails.env.production?
+  # # end
+
+  # # Basic認証の定義
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  #   end
   # end
-
-  # Basic認証の定義
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
 
 end
