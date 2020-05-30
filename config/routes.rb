4e1bd6b540c0ef1798dc70245bc24b40c1d18659
tech-ta <matsllyu@gmail.com>
2020-05-30 19:56:21 +0900

    ルーティング 設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items
+  resources :items do
+    collection do
+      get  'purchase/:id'=>  'items#purchase', as: 'purchase'
+      post 'pay/:id'=>   'items#pay', as: 'pay'
+      get  'done'=>      'items#done', as: 'done'
+    end
+  end
+
+  resources :credit_cards , only: [:new, :create, :destroy]
+
 end
