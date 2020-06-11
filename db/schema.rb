7aba7f35729ad49b4dbce5fdeabd037f26fd2a49
yurarina <yurarina000@gmail.com>
2020-06-11 17:18:34 +0900

    gem 'ancestry'導入、model設定

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_06_06_033805) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
