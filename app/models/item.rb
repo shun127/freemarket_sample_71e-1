68d4d37824cf0d3f1862240bbba9c09c0d6cb994
shun127 <cielo720136@gmail.com>
2020-06-12 11:43:59 +0900

    uodate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_many   :item_images,dependent: :destroy
+  has_many   :comments,dependent: :destroy
+  has_many   :favorites,dependent: :destroy
+  belongs_to :category
+  belongs_to :brand
+  belongs_to :postage_payer,dependent: :destroy
+  belongs_to :preparation_period,dependent: :destroy
+  belongs_to :seller_item,class_name:"User"
+  belongs_to :buyer_item,class_name:"User"
+
+  
 end
