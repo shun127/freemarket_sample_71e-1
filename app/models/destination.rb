fbffe7c1184c6af9c24a234add0b4cb3c8998162
yurarina <yurarina000@gmail.com>
2020-06-20 18:40:37 +0900

    update

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,3 @@
 class Destination < ApplicationRecord
+  
 end
--- db/migrate/20200527131643_create_items.rb
+++ db/migrate/20200527131643_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :name,                         null:false
       t.string :price,                        null:false
       t.text   :item_introduction,            null:false
-      t.string   :condition,                  null:false
+      t.string :condition,                  　null:false
       t.references :seller,                   null:false,foreign_key:{to_table: :users}
       t.references :buyer,                    foreign_key:{to_table: :users}
       t.references :category,                 null:false,foreign_key:true
@@ -12,7 +12,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.references :postage_payers,           null:false,foreign_key:true
       t.references :preparation_period,       null:false,foreign_key:true
       t.string :trading_status,               null:false
-      t.references :item_images,              null: false,foreign_key: true
+      t.references :item_imageas,             null: false,foreign_key: true
       t.string :deal_closed_data
       t.timestamps
     end
