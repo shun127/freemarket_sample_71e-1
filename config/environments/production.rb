8a0b310ef11362a5555c9cee5526719334eeea65
kinopyyy <sss.namiki@gmail.com>
2020-06-26 22:45:08 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  # 本番環境でもローカル開発環境と同じように、ブラウザにエラーメッセージが表示されるようになります。6/26木下
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
