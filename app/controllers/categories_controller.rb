0ed05c07f489d15ae7aa9408c5ab3babe349b7ed
kinopyyy <sss.namiki@gmail.com>
2020-07-07 20:54:39 +0900

    add js

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,5 +7,18 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @items = Item.where(category_id: @category.subtree.ids).order("created_at DESC").page(params[:page]).per(8)
   end  
+
+  def new
+    @children = Category.find(params[:parent_id]).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def set_parents
+    @parents = Category.where(ancestry: nil)
+  end
+
 end
 
