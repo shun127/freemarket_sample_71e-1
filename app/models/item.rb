10b6967af845794f24371c4a09e882f0b0814ce5
mina07yu07 <mina07yu07@gmail.com>
2020-07-07 21:16:49 +0900

    a

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,14 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand, optional: true
   
-  belongs_to :seller_item, class_name:"User", optional: true
-  belongs_to :buyer_item, class_name:"User", optional: true
+  # belongs_to :seller_item, class_name:"User", optional: true
+  belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
+  # belongs_to :buyer_item, class_name:"User", optional: true
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id'
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture, dependent: :destroy
   belongs_to :brand, dependent: :destroy
-  belongs_to :user
+  belongs_to :user, optional: true
 
 end
