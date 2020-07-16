3ce45c138fa968391a649364f4cdf3d391e3b1aa
tech-ta <62540833+tech-ta@users.noreply.github.com>
2020-07-16 21:40:51 +0900

    保留

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -19,9 +19,9 @@ class PurchaseController < ApplicationController
   def pay
     card = CreditCard.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    @item = Item.find_by[params[1]]
+    @item = Item.find_by[params[:id]]
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount =>            @items.price #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
