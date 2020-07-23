4596884769695137113881d1511f11fbb4666cf3
kinopyyy <sss.namiki@gmail.com>
2020-07-23 15:25:32 +0900

    update purchase

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -7,8 +7,7 @@ class CreditCardsController < ApplicationController
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
-    # Payjp.api_key =/ENV["PAYJP_PRIVATE_KEY"]
-    Payjp.api_key = 'sk_test_8c736d594af0a588864c727b'
+    Payjp.api_key =/ENV["PAYJP_PRIVATE_KEY"]
 
     if params['payjp-token'].blank?
 
@@ -36,8 +35,7 @@ class CreditCardsController < ApplicationController
     card = CreditCard.find_by(user_id: current_user.id)
     if card.blank?
     else
-      # Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      Payjp.api_key = 'sk_test_8c736d594af0a588864c727b'
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
