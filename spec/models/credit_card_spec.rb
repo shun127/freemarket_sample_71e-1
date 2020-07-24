704739914e89a809bd2d306e9c09fe28682d6322
kinopyyy <sss.namiki@gmail.com>
2020-07-24 12:31:47 +0900

    修正

---
+++ spec/models/credit_card_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe CreditCard do
+  describe '#create' do
+
+    it "customer_idがないと登録できない" do
+      credit_card = build(:credit_card, customer_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:customer_id]).to include("を入力してください")
+    end
+    
+    it "card_idがないと登録できない" do
+      credit_card = build(:credit_card, card_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:card_id]).to include("を入力してください")
+    end
+  end
+end
